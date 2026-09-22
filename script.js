947550a60a1af87584dd13f96579ea76d5b80781
mtstahdzibunnufus <tu.mtstahdzibunnufus@gmail.com>
2026-09-22 13:28:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Mencari tombol berdasarkan ID yang sudah kita buat tadi
+const tombol = document.getElementById('tombolPesan');
+
+// 2. Memberikan instruksi apa yang terjadi jika tombol diklik
+tombol.addEventListener('click', function() {
+    alert('Halo! Terima kasih telah mengklik tombol ini. JavaScript Anda berhasil berfungsi!');
+});
